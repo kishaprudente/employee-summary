3e2ed55fa27f102368e47159d02aea172c2f3d31
Kisha Prudente <kisha.prudente@gmail.com>
2020-04-26 18:52:06 -0400

    added helper function to create multiple employees at once

--- app.js
+++ app.js
@@ -1,81 +1,104 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-​
-const OUTPUT_DIR = path.resolve(__dirname, "output")
+const inquirer = require("inquirer");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
 const render = require("./lib/htmlRenderer");
-​
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+
 const questions = [
-    {
-        type: "input",
-        name: "name",
-        message: "What is your name?",
+  {
+    type: "input",
+    name: "name",
+    message: "What is your name?",
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "What is your id?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email?",
+  },
+  {
+    type: "list",
+    name: "role",
+    message: "What is your role?",
+    choices: ["Manager", "Engineer", "Intern"],
+  },
+  {
+    type: "input",
+    name: "officeNumber",
+    message: "What is your office number?",
+    when: function (answers) {
+      return answers.role === "Manager";
     },
-    {
-        type: "input",
-        name: "id",
-        message: "What is your id?",
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "What is your GitHub username?",
+    when: function (answers) {
+      return answers.role === "Engineer";
     },
-    {
-        type: "input",
-        name: "email",
-        message: "What is your email?",
+  },
+  {
+    type: "input",
+    name: "school",
+    message: "Where do you go to school?",
+    when: function (answers) {
+      return answers.role === "Intern";
     },
-    {
-        type: "list",
-        name: "role",
-        message: "What is your role?",
-        choices: ["Manager", "Engineer", "Intern"],
-    },
-    {
-        type: "input",
-        name: "officeNumber",
-        message: "What is your office number?",
-        when: function(answers) {
-            return answers.role === "Manager";
-        }
-    },
-    {
-        type: "input",
-        name: "github",
-        message: "What is your GitHub username?",
-        when: function(answers) {
-            return answers.role === "Engineer";
-        }
-    },
-    {
-        type: "input",
-        name: "school",
-        message: "Where do you go to school?",
-        when: function(answers) {
-            return answers.role === "Intern";
-        }
-    }
-]
-​
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-​
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-​
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-​
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
+  },
+  {
+    type: "confirm",
+    name: "again",
+    message: "Add more Employee?",
+    default: true,
+  },
+];
+
+async function createEmployees(employeesInput = []) {
+  try {
+    const { again, ...answers } = await inquirer.prompt(questions);
+    const newEmployee = [...employeesInput, answers];
+    return again ? createEmployees(newEmployee) : newEmployee;
+  } catch (err) {
+    throw err;
+  }
+}
+
+async function init() {
+  try {
+    const employees = [];
+    const employeesData = await createEmployees();
+
+    employeesData.map((employee) => {
+      const { name, id, email, role, officeNumber, github, school } = employee;
+
+      if (role === "Manager") {
+        const newManager = new Manager(name, id, email, officeNumber);
+        employees.push(newManager);
+        console.log(newManager);
+      } else if (role === "Engineer") {
+        const newEngineer = new Engineer(name, id, email, github);
+        employees.push(newEngineer);
+        console.log(newEngineer);
+      } else {
+        const newIntern = new Intern(name, id, email, school);
+        employees.push(newIntern);
+        console.log(newIntern);
+      }
+    });
+    render(employees);
+  } catch (err) {
+    throw err;
+  }
+}
+
+init();
